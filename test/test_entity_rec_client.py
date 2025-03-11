11309ce3b52a177e6cf004e64fb9cc365562dbbe
Eugene233 <agboreugene@gmail.com>
2025-03-12 00:25:55 +0100

    Added test for multiple text options

--- test/test_entity_rec_client.py
+++ test/test_entity_rec_client.py
@@ -60,3 +60,32 @@ class TestEntityRecClient(unittest.TestCase):
         ents = ner.get_ents('...')
         expected_resutl = { "ents": [{'ent': 'English', 'label': 'Language'}] , "html": ''}
         self.assertListEqual(ents["ents"], expected_resutl["ents"])
+
+
+    def test_get_ents_given_multiple_SpaCy_text_is_resturns_multiple_serialized_objects(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [
+            {   
+                'text'      : 'English',
+                'label_'    : 'LANGUAGE'
+            },
+            {   
+                'text'      : 'United Kingdom',
+                'label_'    : 'LOCATION'
+            }  
+        ]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityCLient(model)
+        ents = ner.get_ents('...')
+        expected_resutl = { 
+            "ents": [
+                {
+                    'ent'   : 'English',
+                    'label' : 'Language'
+                },
+                {
+                    'ent'   : 'United Kingdom',
+                    'label' : 'Location'
+                }
+            ] , "html": ''}
+        self.assertListEqual(ents["ents"], expected_resutl["ents"])

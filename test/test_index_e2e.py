66973c389f0279e1f9c9ac661b698961ee40f84a
Eugene233 <agboreugene@gmail.com>
2025-03-12 02:54:12 +0100

    Added e2e test for header

--- test/test_index_e2e.py
+++ test/test_index_e2e.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 class E2ETests(unittest.TestCase):
     
@@ -15,3 +15,13 @@ class E2ETests(unittest.TestCase):
 
     def test_browser_title_contains_app_name(self):
         self.assertIn('Named Entities', self.driver.title)
+
+
+    def test_page_heading_is_text_entity_finder(self):
+        heading = self._find('data-test-heading')
+        self.assertEqual(heading, "Text Entities Finder")
+
+    
+    def _find(self, name):
+        return self.driver.find_element(By.NAME, name).text
+        
